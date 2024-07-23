010b383195f346f9498bc57471c1dda604a10225
@ELTOROIT - Andres Perez <js4Iot@gmail.com>
2024-07-23 15:15:23 -0400

    Cleaned UI a bit

--- src/clientLWC/modules/c/home/home.js
+++ src/clientLWC/modules/c/home/home.js
@@ -19,6 +19,8 @@ export default class Home extends LightningElement {
 
 	onSettingsChanged(event) {
 		this.settings = event.detail;
+		console.log("***", this.settings);
+		// debugger;
 	}
 
 	onResults(event) {
