1a37296c1f05d4f66e23a7f58c6734446970aa53
@ELTOROIT - Andres Perez <andresperezf@yahoo.com>
2024-12-05 18:57:35 -0500

    Try this...

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: npm compileLWC && npm serve
-
+web: npm run serve
--- fullRebuild.sh
+++ fullRebuild.sh
@@ -1,7 +1,7 @@
 rm -rf ./dist
 mkdir dist
-echo "Press <ENTER> to Continue..."
-read
+# echo "Press <ENTER> to Continue..."
+# read
 # cp -r ./src/resources ./dist
 cp ./src/clientLWC/index.html ./dist
 cp -r ./node_modules/@salesforce-ux/design-system/assets/ ./dist/SLDS
